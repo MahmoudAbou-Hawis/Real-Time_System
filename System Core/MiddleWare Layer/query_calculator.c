d5cc22e80e0acd0a63db081f02f8ad15c92058d6
mahmoud1201 <mahmoud.abohawis10@gmail.com>
2023-05-03 14:34:17 +0300

    I changed the path of this file , please add .h file for this file

---
+++ System Core/MiddleWare Layer/query_calculator.c
@@ -0,0 +1,77 @@
+// make .h file for this function and put it in 
+double evaluate_expression(char* expr) {
+    char* ptr = expr;
+    double num_stack[100];
+    char op_stack[100];
+    int num_top = -1;
+    int op_top = -1;
+
+    while (*ptr != '\0') {
+        if (isdigit(*ptr)) {
+            num_top++;
+            num_stack[num_top] = strtod(ptr, &ptr);
+        }
+        else if (ptr == '+' || *ptr == '-' || *ptr == '*' || *ptr == '/') {
+            while (op_top >= 0 && ((op_stack[op_top] == '*' || op_stack[op_top] == '/') || (op_stack[op_top] == '+' || op_stack[op_top] == '-') && (*ptr == '+' || *ptr == '-'))) {
+                double num2 = num_stack[num_top];
+                num_top--;
+                double num1 = num_stack[num_top];
+                num_top--;
+                char op = op_stack[op_top];
+                op_top--;
+                double result = 0.0;
+                switch (op) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                }
+                num_top++;
+                num_stack[num_top] = result;
+            }
+            op_top++;
+            op_stack[op_top] = *ptr;
+            ptr++;
+        }
+        else {
+            // Ignore whitespace
+            ptr++;
+        }
+    }
+
+    while (op_top >= 0) {
+        double num2 = num_stack[num_top];
+        num_top--;
+        double num1 = num_stack[num_top];
+        num_top--;
+        char op = op_stack[op_top];
+        op_top--;
+        double result = 0.0;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+        num_top++;
+        num_stack[num_top] = result;
+    }
+
+    return num_stack[num_top];
+}
