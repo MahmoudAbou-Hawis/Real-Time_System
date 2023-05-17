00f69292586e72aa2a6e9c485720e5bab75179dd
Ali Ashraf <73755654+aliashraf58@users.noreply.github.com>
2023-05-17 14:30:43 +0300

    last product
    in this version, we added the face recognition to the server and connected it to the web page

--- Desktop Application/OTP Server/main.py
+++ Desktop Application/OTP Server/main.py
@@ -1,76 +1,179 @@
-import http.server
-import socketserver
 import socket
-from twilio.rest import Client
-import os
-from twilio.rest import Client
-
-account_sid = ""
-auth_token = ""
-
-
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import cv2
+import numpy as np
+import face_recognition
+import math
+import os, sys
 
 
-temp_client = Client(account_sid, auth_token)
-service = temp_client.verify.v2.services.create(
-                                        friendly_name='SquadRTOS system'
-                                    )
-verify_sid = service.sid
+class FaceRecognition:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
 
-def send_otp(NumberPhone):
-    global verified_number
-    verified_number = NumberPhone
-    verification = temp_client.verify \
-        .services(verify_sid) \
-        .verifications \
-        .create(to=verified_number, channel="sms")
-    print(verification.status)
+    def __init__(self):
+        self.encode_faces()
 
-def verify_otp(OTP):
-    print(verified_number)
-    verification_check = temp_client.verify.v2.services(verify_sid) \
-        .verification_checks \
-        .create(to=verified_number, code=OTP)
-    return (verification_check.status)
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face_image = face_recognition.load_image_file(f"faces/{image}")
+            face_encoding = face_recognition.face_encodings(face_image)[0]
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def Response200(self):
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
+
+    def run_recognition(self, Frame):
+
+        try:
+            # Capture a frame
+            frame = Frame
+
+            # Only process every other frame of video to save time
+            if self.process_current_frame:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                # Find all the faces and face encodings in the current frame of video
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = "Unknown"
+                    confidence = '???'
+
+                    # Calculate the shortest distance to face
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        confidence = face_confidence(face_distances[best_match_index])
+
+                    self.face_names.append(f'{name} ')
+
+            self.process_current_frame = not self.process_current_frame
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Create the frame with the name
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+
+            # Display the resulting image
+
+            #cv2.imshow('Face Recognition', frame)
+            #cv2.waitKey(0)
+            return name
+            # Hit 'q' on the keyboard to quit!
+
+            # with open('myfile.txt', 'w') as f:
+            #     f.write("start\n")
+            #     if name != "Unknown (???)":
+            #         f.write(name)
+            #     else:
+            #         f.write("off")
+
+        except:
+            pass
+        # Release handle to the webcam
+        cv2.destroyAllWindows()
+
+
+fr = FaceRecognition()
+
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
+
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+
+
+class myRequestHandler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
         self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header('Access-Control-Max-Age', '86400')
         self.end_headers()
-    def do_GET(self):
-        print(self.path)
-        API = ''
-        for chr in self.path:
-            if chr == '?':
-                break
-            else:
-                API+=chr
-       # print('Headers :',self.headers)
-        params = self.path.split('?')
-        if len(params) > 1:
-            params = params[1].split('&')
-            params = {p.split('=')[0]: p.split('=')[1] for p in params}
+
+    def do_POST(self):
+        content_length = int(self.headers.get('Content-Length'))
+        data = self.rfile.read(content_length)
+        np_array = np.frombuffer(data, dtype=np.uint8)
+        frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        result = fr.run_recognition(frame)
+
+        # Send response
+        self.send_response(200)
+
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        print(result)
+        if result != 'Unknown':
+            self.wfile.write(b'Unknown.')
         else:
-            params = {}
-        if str(API) == '/send_otp':
-            send_otp('+2'+params['phone'])
-            self.Response200()
-            self.wfile.write(b'OTP Sent')
-        elif str(API) == '/check_otp':
-           status = verify_otp(str(params['OTP']))
-           if status == 'pending' :
-               self.Response200()
-               self.wfile.write(b'Wrong OTP!')
-           elif status == 'approved':
-               self.Response200()
-               self.wfile.write(b'Accepted!')
+            self.wfile.write(result)
+
+    def do_GET(self):
+        client_ip = self.client_address[0]
+        print("Received request from", client_ip)
+        print(self.path)
+        with open('myfile.txt', 'r') as f:
+            file_content = f.read()
+            search_string = "off"
+            if search_string in file_content:
+                message = "not found"
+                status_code = 200
+            else:
+                message = "found"
+                status_code = 404
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode('utf-8'))
 
 
-PORT = 8080
-
-with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    ip_address = socket.gethostbyname(socket.gethostname())
-    print(f"Serving at http://{ip_address}:{PORT}/")
+def run():
+    port = 8070
+    host = ''
+    for iface in socket.if_nameindex():
+        if iface[1] == 'lo':
+            continue
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        host = s.getsockname()[0]
+        s.close()
+        break
+    if not host:
+        print('Error: could not determine IP address to bind to')
+        return
+    httpd = HTTPServer((host, port), myRequestHandler)
+    print('Server started at http://{}:{}/'.format(host, port))
     httpd.serve_forever()
+
+
+run()
